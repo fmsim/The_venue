12e2129f120ea3a4c6d2f24eab20959a99a0a3f2
Frank Simon <f.m.sim@t-online.de>
2022-03-07 20:22:16 +0100

    added carousel with react-slick and slick-carousel

---
+++ src/components/featured/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Carousel from "./Carousel"
+
+const Featured = () => {
+  return (
+    <div className="featured-container">
+      <Carousel />
+      <div className="artist_name">
+        <div className="wrapper">Ariana Grande</div>
+      </div>
+    </div>
+  )
+}
+
+export default Featured
